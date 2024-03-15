b6f889190c9b83b21f4896ade688621f7e7830a2
Niranjan02 <niranjanmj02@gmail.com>
2024-03-15 17:25:46 +0530

    nir

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import logoImg from '../assets/logo.jpg';
 import { useContext } from 'react';
 import CartContext from '../store/CartContext';
 import UserProgressContext  from '../store/UserProgressContext';
+import UserProgressContexts from '../store/SubscriptionContext';
 
 debugger
 export default function Header(){
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
+    const userProgressCtxs = useContext(UserProgressContexts)
+
 
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
      return totalNumberOfItems + item.quantity;
@@ -15,7 +18,12 @@ export default function Header(){
 
     function handleShowCart(){
         userProgressCtx.showCart();
-    }   
+    } 
+    function handleSubs(){
+        userProgressCtxs.showCart();
+
+    }
+
 
     return <header id="main-header"> 
     <div id="title">
@@ -23,8 +31,11 @@ export default function Header(){
         <h1>React Food</h1>
     </div>
     <nav>
-        <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
-        <Button textOnly onClick={handleShowCart}>Subscription </Button>
+        <Button textOnly onClick={handleShowCart}>Cart/Subscribe ({totalCartItems})</Button>
+        
     </nav>
     </header>
 }
+
+
+//<Button textOnly onClick={handleShowCart}>Subscription </Button>
